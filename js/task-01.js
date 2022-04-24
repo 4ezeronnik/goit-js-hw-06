6376eb33c6840cb9af1a8b084b23effaa187386d
Andrii Reznichenko <4ezeronnik@gmail.com>
2022-04-24 23:10:02 +0300

    added the 'animals'

--- js/task-01.js
+++ js/task-01.js
@@ -1,2 +1,12 @@
-const elementsOfList = document.querySelector('ul');
-console.log(elementsOfList);
+const elementsOfList = document.querySelectorAll('li.item');
+console.log('Number of categories:', elementsOfList.length);
+
+console.log('');
+
+const animalsOfList = document.querySelector('h2');
+console.log('Category:', animalsOfList.textContent);
+
+const amountOfAnimals = document.querySelector('li.item > ul');
+const amountOfAnotherAnimals = amountOfAnimals.children;
+console.log('Elements:', amountOfAnotherAnimals.length);
+
