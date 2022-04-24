dc244d35d4406c9b7fe937a336a74b08333ebbe6
Andrii Reznichenko <4ezeronnik@gmail.com>
2022-04-24 23:41:33 +0300

    Update Task 1

--- js/task-01.js
+++ js/task-01.js
@@ -10,3 +10,35 @@ const amountOfAnimals = document.querySelector('li.item > ul');
 const amountOfAnotherAnimals = amountOfAnimals.children;
 console.log('Elements:', amountOfAnotherAnimals.length);
 
+console.log('');
+
+const productsOfList = document.querySelectorAll('h2');
+console.log('Category:', productsOfList[1].textContent);
+
+const amountOfProducts = document.querySelectorAll('li.item > ul');
+const amountOfAnotherProducts = amountOfProducts[1];
+
+const amountOfFinalProducts = amountOfAnotherProducts.children;
+console.log('Elements:', amountOfFinalProducts.length);
+
+console.log('');
+
+const technologiesOfList = document.querySelectorAll('h2');
+console.log('Category:', technologiesOfList[2].textContent);
+
+const amountOfTechnologies = document.querySelectorAll('li.item > ul');
+const amountOfAnotherTechnologies = amountOfTechnologies[2];
+
+const amountOfFinalTechnologies = amountOfAnotherTechnologies.children;
+console.log('Elements:', amountOfFinalTechnologies.length);
+
+
+
+
+
+
+
+
+
+
+
