2414f0aa44c4011e219d28f1c79bdaac5f00f571
Andrii Reznichenko <4ezeronnik@gmail.com>
2022-04-25 22:45:50 +0300

    Updated task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,43 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const potatoesEl = document.createElement('li');
+potatoesEl.textContent = 'Potatoes';
+potatoesEl.classList.add('item');
+console.log(potatoesEl.textContent);
+
+const mushroomsEl = document.createElement('li');
+mushroomsEl.textContent = 'Mushrooms';
+mushroomsEl.classList.add('item');
+console.log(mushroomsEl.textContent);
+
+const garlicEl = document.createElement('li');
+garlicEl.textContent = 'Garlic';
+garlicEl.classList.add('item');
+console.log(garlicEl.textContent);
+
+const tomatosEl = document.createElement('li');
+tomatosEl.textContent = 'Tomatos';
+tomatosEl.classList.add('item');
+console.log(tomatosEl.textContent);
+
+const herbsEl = document.createElement('li');
+herbsEl.textContent = 'Herbs';
+herbsEl.classList.add('item');
+console.log(herbsEl.textContent);
+
+const condimentsEl = document.createElement('li');
+condimentsEl.textContent = 'Condiments';
+condimentsEl.classList.add('item');
+console.log(condimentsEl.textContent);
+
+const listOfIngredients = document.querySelector('ul#ingredients');
+
+
+listOfIngredients.append(potatoesEl, mushroomsEl, garlicEl, tomatosEl, herbsEl, condimentsEl);
+console.log(listOfIngredients);
+
+
+
+
