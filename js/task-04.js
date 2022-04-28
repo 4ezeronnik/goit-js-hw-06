bc59d2e8d3f656e2cc81dd7a20f2b11319b4edf3
Andrii Reznichenko <4ezeronnik@gmail.com>
2022-04-28 12:32:42 +0300

    update 04 task

--- js/task-04.js
+++ js/task-04.js
@@ -1,30 +1,58 @@
+// const decrementButton = document.querySelector('button[data-action="decrement"]');
+
+
+// const incrementButton = document.querySelector('button[data-action="increment"]');
+
+// const counterValue = document.querySelector('span');
+
+
+// const counter = {
+//     value: 0,
+//     increment() {
+//         this.value += 1;
+//     },
+//     decrement() {
+//         this.value -= 1;
+//     }
+// }
+
+// decrementButton.addEventListener('click', function () { 
+//     counter.decrement();
+
+//     counterValue.textContent = counter.value;
+// });
+
+// incrementButton.addEventListener('click', function () { 
+//     counter.increment();
+//     counterValue.textContent = counter.value;
+// });
+
+
 const decrementButton = document.querySelector('button[data-action="decrement"]');
 
 
 const incrementButton = document.querySelector('button[data-action="increment"]');
 
-const counterValue = document.querySelector('span');
+const counterDisplayElem = document.querySelector('#value');
 
+let counterValue = 0;
 
-const counter = {
-    value: 0,
-    increment() {
-        this.value += 1;
-    },
-    decrement() {
-        this.value -= 1;
-    }
-}
+updateDisplay();
 
-decrementButton.addEventListener('click', function () { 
-    counter.decrement();
-
-    counterValue.textContent = counter.value;
+ decrementButton.addEventListener('click', () => {
+    counterValue -= 1;
+    updateDisplay();
 });
 
-incrementButton.addEventListener('click', function () { 
-    counter.increment();
-    counterValue.textContent = counter.value;
-});
+ incrementButton.addEventListener('click', () => {
+    counterValue += 1;
+    updateDisplay();
+})
+
+
+
+function updateDisplay() {
+    counterDisplayElem.innerHTML = counterValue;
+};
 
 
