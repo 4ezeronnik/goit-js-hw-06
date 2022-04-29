f41a1f6eea08ba4ea22da3e4e6e5925a5ebc5632
Andrii Reznichenko <4ezeronnik@gmail.com>
2022-04-29 10:14:35 +0300

    update 05 task

--- js/task-05.js
+++ js/task-05.js
@@ -2,8 +2,10 @@ const textInput = document.querySelector('input#name-input');
 const output = document.querySelector('span#name-output');
 
 textInput.addEventListener('input', (event) => {
-   output.textContent = event.currentTarget.value ;
-    
+   output.textContent = event.currentTarget.value;
+   if (!event.currentTarget.value) {
+      output.textContent = 'Anonymus';
+  }
 });
     
     
