f902fd29a283a907c73624b8fac13647015c54f6
Andrii Reznichenko <4ezeronnik@gmail.com>
2022-04-30 11:27:51 +0300

    updated 08 task

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,15 @@
+const formRef = document.querySelector('.login-form');
 
+formRef.addEventListener('submit', onSubmit);
+
+function onSubmit(event) {
+    event.preventDefault();
+
+    const { email, password } = event.currentTarget;
+
+    if (email.value === "" || password.value === "") {
+        alert('Please, fill in all the fields');
+    }
+    console.log(`Email: ${email.value}, Password: ${password.value}`);
+    event.currentTarget.reset();
+}
