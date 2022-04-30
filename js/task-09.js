7ea01551361cbf60c13896b54912c4d167601976
Andrii Reznichenko <4ezeronnik@gmail.com>
2022-04-30 12:00:51 +0300

    updated 9 task

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
+const buttonRef = document.querySelector('button.change-color');
+const colorOutputRef = document.querySelector('span.color');
+
+buttonRef.addEventListener('click', onButtonClick);
+
+
+function onButtonClick() {
+  document.body.style.background = getRandomHexColor();
+  colorOutputRef.textContent = document.body.style.background;
+  
+}
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
