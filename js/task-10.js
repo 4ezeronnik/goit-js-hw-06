848c3a88032a295f7fec61381ea62414b7aeea20
Andrii Reznichenko <4ezeronnik@gmail.com>
2022-04-30 15:34:22 +0300

    Update 10 task

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,26 @@
+const inputRef = document.querySelector('input');
+const btnCreateRef = document.querySelector('button[data-create="Create"]')
+const btnDestroyRef = document.querySelector('button[data-destroy="Destroy"]'); 
+const divBoxesRef = document.querySelector('div#boxes');
+
+
+inputRef.addEventListener('click', onInputClick);
+
+function onInputClick() {
+  
+}
+
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+
+function createBoxes(amount) {
+
+}
+
+function destroyBoxes() {
+
 }
